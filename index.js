870702af794f0e6748b2ddffa05bb3d7d0dea887
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-03-31 16:24:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function formatDate(object) {
+//  vysvětlení funkce "padStart()" zde: https://www.w3schools.com/jsref/jsref_string_padstart.asp
+    const day = object.day.toString().padStart(2, "0")
+    const month = object.month.toString().padStart(2, "0")
+    const year = object.year.toString().padStart(4, "0")
+
+    return `${day}-${month}-${year}`
+}
+
+console.log(formatDate({ day: 12, month: 3, year: 2015 }))
